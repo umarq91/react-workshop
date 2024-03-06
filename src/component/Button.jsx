7ffd41951df19f83bc67f594ecc2b5510e15a656
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2024-03-06 19:03:45 +0500

    useState

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Button = ({color,children,onClick}) => {
+   const [count, setCount]  = useState(0)
+
 
 
   return (
-    <button  onClick={onClick} className={`ml-4 bg-${color}-500 text-white px-2 py-1 rounded-md hover:bg-${color}-400`}>{children}</button>
+    <>
+    <button onClick={onClick} className={`ml-4 bg-${color}-500 text-white px-2 py-1 rounded-md hover:bg-${color}-400`}>{children} </button>
+    </>
   )
 }
 
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 
 const Card = (props) => {
     const {img,profile,title="Your title",channel,views,time,verified} = props
 
-
+    const [subscribe,setSubscribe] = useState("Subscribe")
+    const [like,setLiked] = useState("Like")
+  const [isLiked , setIsLiked ] = useState(false)
     function handleSubscribe(){
-      console.log( "Subscribed")
+  
+      if(subscribe === "Subscribe"){
+        setSubscribe("Subscribed")
+      }else{
+        setSubscribe("Subscribe")
+      }
+// subscribe= "subscribed"
   }
 
   function handleLike(){
-    console.log( "Liked")
+
+    if(!isLiked){
+    setLiked("Liked")
+      setIsLiked(true)
+  }else{
+    setLiked("Like")
+    setIsLiked(false)
+  }
+
+    
 }
 
+// function handleInput(e){
+// setSubscribe(e.target.value)
+// }
   return (
     <div className="w-[85%] max-w-sm rounded-2xl  overflow-hidden shadow-lg h-[400px]">
     <a className="aspect-video overflow-hidden rounded-t-2xl group relative" href="#">
@@ -29,6 +49,7 @@ const Card = (props) => {
       />
       <div className="w-10 h-10 absolute inset-0 m-auto translate-x-2/4 text-gray-900 group-hover:text-gray-100" />
     </a>
+  
     <div className="p-4">
       <a className="text-base font-semibold line-clamp-2 hover:underline" href="#">
         {title}
@@ -43,9 +64,9 @@ const Card = (props) => {
     </div>
     {/* <button onClick={()=>console.log("Subscribed")} className='ml-4 bg-red-500 text-white px-2 py-1 rounded-md hover:bg-red-400'> Subscribe </button>
     <button onClick={()=>console.log("Liked")} className='ml-4 bg-blue-500 text-white px-4 py-1 rounded-md hover:bg-blue-400'> Like </button> */}
-  <Button onClick={handleSubscribe}  color="red"> Subscribe </Button>
-  <Button  onClick={handleLike} color="blue"> Like </Button>
-
+  <Button onClick={handleSubscribe}   color="red"> {subscribe} </Button>
+  <Button  onClick={handleLike} color="blue"> {like} </Button>
+  {/* <input onChange={(e)=>handleInput(e)} type="text" /> */}
   </div>
   )
 }
